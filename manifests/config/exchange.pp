cde15d84e3e901a8a75fbedd2f7272475846f62e
Justice London <jlondon@syrussystems.com>
2013-06-20 12:50:42 -0400

    Added exchange configuration.

---
+++ manifests/config/exchange.pp
@@ -0,0 +1,28 @@
+class rabbitmq::config::exchange (
+  $vhost      = '/',
+  $type       = 'direct',
+  $webui_port = '15672',
+  $name,
+) {
+
+  #Install rabbitmqadmin to the host
+  exec { 'rabbitmqadmin-download':
+    command => 'wget http://localhost:15672/cli/rabbitmqadmin -O /usr/local/bin/rabbitmqadmin',
+    path    => ['/usr/bin', '/bin', '/sbin', '/usr/sbin'],
+    creates => '/usr/local/bin/rabbitmqadmin',
+  } 
+  file { '/usr/local/bin/rabbitmqadmin':
+    owner   => root,
+    group   => root,
+    mode    => '0655',
+    require => Exec['rabbitmqadmin-download'],
+  } 
+ 
+  #Run the exchange creation command
+  exec { 'rabbitmqadmin-exchange':
+    command => "rabbitmqadmin declare exchange --vhost=${vhost} name=${name} type=${type}",
+    path    => ['/usr/local/bin','/bin','/sbin','/usr/bin','/usr/sbin'],
+    require => File['/usr/local/bin/rabbitmqadmin'],
+  }
+
+} 
